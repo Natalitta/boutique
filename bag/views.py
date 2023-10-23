c846c457da79b12e374a133474e120cf9f401974
Natalitta <nataliialebedieva@gmail.com>
2023-10-23 21:05:38 +0000

    Add css for toasts

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
-from products.model import Product
+from products.models import Product
 
 def view_bag(request):
     # to view shopping bag page
@@ -32,7 +32,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
-            message.success(request, f'Added {product.name} to your bag')
+            messages.success(request, f'Added {product.name} to your bag')
     
     request.session['bag'] = bag
     return redirect(redirect_url)
