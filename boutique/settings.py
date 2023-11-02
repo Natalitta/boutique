af3e6c1ed09face1a9e08874f26ad93d76e020db
Natalitta <nataliialebedieva@gmail.com>
2023-11-02 23:34:10 +0000

    Add confirmation emails functionality

--- boutique/settings.py
+++ boutique/settings.py
@@ -181,4 +181,5 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
