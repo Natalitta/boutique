52de126dceb2b41293681a9a888d45f5f3f4efc0
Natalitta <nataliialebedieva@gmail.com>
2023-11-03 15:44:32 +0000

    Remove secret key and set debug

--- boutique/settings.py
+++ boutique/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3-e-7$^bc%1oe0eek)of_to&1vndx93#qyuq1c1p&_lu2vcvm@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-natalitta-boutique-yd2q1mwh6mv.ws-eu105.gitpod.io',
                 'boutique-app-1acb4093fa38.herokuapp.com'
