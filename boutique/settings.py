3d4fc3cb1978fc86daea4ded69cb98967755f3af
Natalitta <nataliialebedieva@gmail.com>
2023-11-03 15:31:48 +0000

    Deployment

--- boutique/settings.py
+++ boutique/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,9 @@ SECRET_KEY = 'django-insecure-3-e-7$^bc%1oe0eek)of_to&1vndx93#qyuq1c1p&_lu2vcvm@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-natalitta-boutique-yd2q1mwh6mv.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['8000-natalitta-boutique-yd2q1mwh6mv.ws-eu105.gitpod.io',
+                'boutique-app-1acb4093fa38.herokuapp.com'
+                ]
 
 
 # Application definition
@@ -119,12 +122,17 @@ WSGI_APPLICATION = 'boutique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
--- products/views.py
+++ products/views.py
@@ -131,7 +131,7 @@ def edit_product(request, product_id):
 @login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
-     if not request.user.is_superuser:
+    if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
         
